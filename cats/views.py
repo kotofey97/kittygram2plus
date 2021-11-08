8038231e9081320ff700d94429e66614b4ac4459
kotofey97 <olya_sitnikova@mail.ru>
2021-11-08 14:46:41 +0300

    Тротлинг

--- cats/views.py
+++ cats/views.py
@@ -1,14 +1,17 @@
 from rest_framework import viewsets
-
+from rest_framework.throttling import ScopedRateThrottle
 from .models import Achievement, Cat, User
 
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 from .permissions import OwnerOrReadOnly, ReadOnly
+from .throttling import WorkingHoursRateThrottle
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
     permission_classes = (OwnerOrReadOnly,)
+    throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
+    throttle_scope = 'low_request'
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
