ba46d1740e205833d486f80384405f342f25b67f
kotofey97 <olya_sitnikova@mail.ru>
2021-11-08 18:22:27 +0300

    фильтрафия, поиск, сортировка

--- cats/views.py
+++ cats/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.throttling import ScopedRateThrottle
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 from .models import Achievement, Cat, User
 
@@ -8,13 +10,22 @@ from .permissions import OwnerOrReadOnly, ReadOnly
 from .throttling import WorkingHoursRateThrottle
 from .pagination import CatsPagination
 
+
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
     permission_classes = (OwnerOrReadOnly,)
-    pagination_class = CatsPagination
-    throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
-    throttle_scope = 'low_request'
+    # pagination_class = CatsPagination
+    # throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
+    # throttle_scope = 'low_request'
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,
+                       filters.OrderingFilter)
+    filterset_fields = ('color', 'birth_year')
+    pagination_class = None
+    # search_fields = ('name',)
+    search_fields = ('achievements__name', 'owner__username')
+    ordering_fields = ('name', 'birth_year')
+    ordering = ('birth_year',)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
