a60ffb25acd0b637181ecf76f67b74d6c84f0474
kotofey97 <olya_sitnikova@mail.ru>
2021-11-08 15:03:13 +0300

    Пагинация

--- cats/views.py
+++ cats/views.py
@@ -1,15 +1,18 @@
 from rest_framework import viewsets
 from rest_framework.throttling import ScopedRateThrottle
+
 from .models import Achievement, Cat, User
 
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 from .permissions import OwnerOrReadOnly, ReadOnly
 from .throttling import WorkingHoursRateThrottle
+from .pagination import CatsPagination
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
     permission_classes = (OwnerOrReadOnly,)
+    pagination_class = CatsPagination
     throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
     throttle_scope = 'low_request'
 
